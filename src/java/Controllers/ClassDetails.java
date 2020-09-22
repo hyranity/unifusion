4e5eced75b4f4f80bef23f4e456deefc553286aa
mast3 <mast3@LAPTOP-JBLBSL11>
2020-09-22 10:03:47 +0800

    Class Details page, no edits yet

---
+++ src/java/Controllers/ClassDetails.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import Models.*;
+import Util.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.UserTransaction;
+
+/**
+ *
+ * @author mast3
+ */
+@WebServlet(name = "ClassDetails", urlPatterns = {"/ClassDetails"})
+public class ClassDetails extends HttpServlet {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Resource
+    private UserTransaction utx;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        // Setup
+        Servlet servlet = new Servlet(request, response);
+        DB db = new DB(em, utx);
+        Models.Users user = Server.getUser(request, response);
+
+        // Get class from DB
+        String classCode = servlet.getQueryStr("class");
+        Query query = em.createNativeQuery("select * from class c, classparticipant cpa, participant p where c.classID = ? and cpa.classid = c.classID and cpa.participantid = p.participantid and p.userid = ?", Models.Class.class).setParameter(1, classCode).setParameter(2, user.getUserid());
+        ArrayList<Models.Class> result = db.getList(Models.Class.class, query);
+
+        if (result.size() == 0) {
+            // If no results
+            System.out.println("Null class");
+        } else {
+            // If a class is found
+            Models.Class classroom = result.get(0);
+            servlet.putInJsp("class", classroom);
+            servlet.servletToJsp("classDetails.jsp");
+        }
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
